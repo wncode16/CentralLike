bed33e776f1e7e38b8ffc8e497fe12b53e3cb8ac
wncode16 <wncode16@gmail.com>
2026-01-17 03:44:22 -0300

    Create http.js

---
+++ functions/_lib/http.js
@@ -0,0 +1,10 @@
+export function json(data, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: { 'Content-Type': 'application/json' }
+  });
+}
+
+export function badRequest(msg) {
+  return json({ error: msg }, 400);
+}
